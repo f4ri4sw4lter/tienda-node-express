efe758dd4af136486f9e03df054d2d0901dad665
f4ri4sw4lter <fariaswalter95@gmail.com>
2023-02-22 23:17:53 -0300

    Se agrega el panel para configurar un producto

--- components/backoffice/controller.js
+++ components/backoffice/controller.js
@@ -33,27 +33,37 @@ const toggleDestacado = (req, res, next) => {
     var { params } = req;
     try{
         Db.query("SELECT destacado FROM productos WHERE id ="+params.id, function (err,result){
-            console.log(result);
             var destacado  = result[0].destacado;
             (destacado==0) ? destacado++ : destacado--;
-            console.log(destacado);
             try{
                 Db.query("UPDATE productos SET destacado = "+destacado+" WHERE id="+params.id, function(err){
                     try {
                         Db.query("SELECT * FROM productos", function (err, resultados) {
                             if (err) {
-                                console.log(err)
+                                console.log(err);
                             } else {
                                 res.render('backoffice/productos', { titulo: 'Productos', productos: resultados });
                             }
                         });
                     } catch (err) {
-                        console.log(err)
+                        console.log(err);
                     }
                 })
             }catch(err){
-                console.log(err)
+                console.log(err);
             }
+        })
+    } catch (err){
+        console.log(err);
+    }
+}
+
+const panelProducto = (req, res, next) => {
+    var { params } = req;
+    try{
+        Db.query("SELECT * FROM productos WHERE id ="+params.id, function (err,producto){
+            console.log(producto);
+            res.render('backoffice/producto', { titulo: 'Producto', producto: producto[0] });
         })
     } catch (err){
 
@@ -64,4 +74,5 @@ module.exports.BackofficeController = {
     dashboard,
     panelProductos,
     toggleDestacado,
+    panelProducto
 }
--- components/backoffice/routes.js
+++ components/backoffice/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { BackofficeController } = require('./controller')
 
 router.get('/', BackofficeController.dashboard)
+      .get('/producto/:id', BackofficeController.panelProducto)
       .get('/productos', BackofficeController.panelProductos)
       .get('/productos/toggledestacado/:id', BackofficeController.toggleDestacado)
 
